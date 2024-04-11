f58e9eb16fc93c6ee50b44698b5ffa26df6423f5
VineeTagarwaL-code <vineetagarwal.now@gmail.com>
2024-04-11 18:00:13 +0530

    feat: added cursor follower

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,39 @@
 import { Navbar } from "@/components/navbar";
 import { ResumeButton } from "@/components/resume-button";
 import { Introduction } from "@/components/introduction";
-
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+   const [mousePosition , setMousePosition] = useState({x: 0, y: 0})
+
+   useEffect(() => {
+    const mouseMove = (e:any) => {
+      setMousePosition({x: e.clientX, y: e.clientY})
+    }
+
+    window.addEventListener("mousemove", mouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    }
+  }, []);
 
   return (
-
     <main className="min-h-screen relative">
        <Navbar />
        <ResumeButton/>
        <Introduction/>
-
+       <motion.div
+        transition={{ 
+          type: "spring", 
+          damping: 15, // Adjust damping for smoother motion
+          stiffness: 100 // Adjust stiffness for smoother motion
+        }}
+         whileHover={{ scale: 0.9 }} // Scale on hove
+        className="absolute w-4 h-4 border-[2px] border-solid border-gray-200/40 p-3 rounded-full pointer-events-none z-[100000]"
+        style={{ top: mousePosition.y-10, left: mousePosition.x-15 }}
+      />
     </main>
   );
 }
--- components/introduction.tsx
+++ components/introduction.tsx
@@ -19,17 +19,17 @@ export const Introduction = () => {
             height={400}
             className="rounded-[3rem] z-[10] relative shadow-md shadow-black"
           />
-          <div className="w-[400px] h-[400px] bg-foreground absolute top-3 left-3 rounded-[3rem] z-[0] shadow-md shadow-black" />
+         
         </div>
       </div>
       <div className="flex justify-between items-center w-full flex-col md:flex-row gap-8">
       <div className="md:w-[50%] w-full">
-        <p className="font-jetbrain mb-4 text-2xl text-foreground md:hidden">activity</p>
+        <p className="font-jetbrain mb-4 text-2xl text-sky-200/95 md:hidden">activity</p>
       <DiscordPresence/>
       </div>
       <div className="md:w-[50%]">
-      <p className="font-jetbrain mb-4 text-2xl text-foreground md:hidden">bio</p>
-        <div className="font-grotesk  text-md md:text-xl md:ml-6 select-none flex-nowrap">Hey, I{`'`}m Vineet I{`'`}m a <Tip name="20" tip="yes thats correct"/> year old full stack developer and freelancer based in india, I{`'`}ve been coding since 2019 and have been freelancing and working since 
+      <p className="font-jetbrain mb-4 text-2xl text-sky-200/95 md:hidden">bio</p>
+        <div className="font-grotesk  text-md md:text-xl md:ml-6 select-none flex-nowrap">Hey, I{`'`}m Vineet I{`'`}m a <Tip name="20" tip="yes thats correct"/> year old full stack developer and freelancer based in India, I{`'`}ve been coding since 2019 and have been freelancing and working since 
         2022. I also <Tip name="love" tip="<3"/> contributing to open source to give back to the community</div>
       </div>
       </div>
