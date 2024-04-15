813dadf95d51d468e8a3382edb2e55effb7a6f96
VineeTagarwaL-code <vineetagarwal.now@gmail.com>
2024-04-16 01:29:57 +0530

    fix: added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Space_Grotesk, JetBrains_Mono } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
 import Head from "next/head";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { useEffect } from "react";
@@ -31,6 +31,7 @@ export default function RootLayout({
     <html lang="en">
       <Head>
         <link rel="icon" href="/image.ico" sizes="32x32" />
+        <script defer src="https://analytics.us.umami.is/script.js" data-website-id="c3b5ab9c-c688-4432-af2c-e04fe5779ca5"></script>
       </Head>
       <body
         className={cn(
