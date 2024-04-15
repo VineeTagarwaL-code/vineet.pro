4e32d19f5095f4881eae78a428ad42b93d674052
VineeTagarwaL-code <vineetagarwal.now@gmail.com>
2024-04-16 01:22:08 +0530

    fix: typo

--- components/experience.tsx
+++ components/experience.tsx
@@ -14,7 +14,7 @@ const experience: experienceType[] = [
     description: [
       `Developed a prototype to integrate real-time Discord announcements into the extension for enhanced functionality,
             Resulting a 25% boost in user retention.`,
-      "Optimizatized backend, reducing response time by 100ms and enhancing flexibility for future modifications.",
+      "Optimitized backend, reducing response time by 100ms and enhancing flexibility for future modifications.",
       "Improved user experience and retention by 50% through UI issue resolution and enhanced responsiveness.",
     ],
     lang: [
